c758622a4c69156f33d1df79ac8a3b44fa2b196f
davi <daviforever17@gmail>
2024-11-25 21:11:50 -0300

    astros

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,44 @@
 import './index.scss';
+import { useState } from 'react';
 
-export default function Home(){
+export default function Home() {
 
-  return(
+  const [active, setActive] = useState('Galáxias');
 
-    <div className=''>
+  const navItems = ['Galáxias', 'Astronomos', 'Planetas'];
 
+  return (
+
+    <div className='home-page'>
+
+      <img src="/assets/images/uiverse-astro.jpg" alt="" />
+
+      <header>
+
+        {navItems.map(item => 
+
+          <a
+            key={item}
+            className={`${active === item ? 'active-link' : 'link'}`}
+            onClick={() => setActive(item)}>
+            {item}
+          </a>
+  
+        )}
+
+      </header>
+
+      {active == 'Galáxias' &&
+
+        <div className='nav-galaxys'>
+
+          
+
+        </div>
+
+      }
+
+      
 
     </div>
 
